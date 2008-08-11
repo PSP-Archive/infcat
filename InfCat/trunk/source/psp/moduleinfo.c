e7b2fa1080a17cef32e50b5dd0f913055973579e
sofiyacat <sofiyacat@8b41f346-dd3e-0410-aa17-a026b9a4d357>
2008-08-11 02:18:21 +0000

    ・ディレクトリ追加 ・アイコンイメージ

---
+++ InfCat/trunk/source/psp/moduleinfo.c
@@ -0,0 +1,8 @@
+#include <pspmoduleinfo.h>
+#include <pspthreadman.h>
+
+PSP_MODULE_INFO( "InfCat", PSP_MODULE_USER, 1, 1);
+PSP_MAIN_THREAD_ATTR(PSP_THREAD_ATTR_USER);
+
+PSP_HEAP_SIZE_MAX();
+PSP_MAIN_THREAD_STACK_SIZE_KB(128);
